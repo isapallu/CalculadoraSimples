e15f305b54fc71c1db5750f479e19ab8dc18c285
cristian_p_santos <cristian_p_santos@estudante.sesisenai.org.br>
2023-03-23 11:01:58 -0300

    drawable

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -10,20 +10,20 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView resultado;
-    EditText numero1, numero2;
+    EditText numero1;
     Button btnAdicao, btnSubtracao, btnDivisao, btnMultiplicacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-        numero1 = findViewById(R.id.numero1);
+        numero1 = findViewById(R.id.numero2);
         numero2 = findViewById(R.id.numero2);
-        resultado = findViewById(R.id.resultado);
+        resultado = findViewById(R.id.resultado)
         btnAdicao = findViewById(R.id.btnAdicao);
         btnSubtracao = findViewById(R.id.btnSubtracao);
         btnDivisao = findViewById(R.id.btnDivisao);
-        btnMultiplicacao = findViewById(R.id.btnMultiplicacao);
+        btnMultiplicacao = findViewById(R.id.btnMultiplicacao)
     }
     public void btnAdicao(View v){
         int intNumero1 = Integer.parseInt(numero1.getText().toString());
@@ -31,22 +31,22 @@ public class MainActivity extends AppCompatActivity {
         int soma = intNumero1 + intNumero2;
         String resultadoSoma = String.valueOf(soma);
         resultado.setText(resultadoSoma);
-    }
+
     public void btnSubtracao(View v){
-        int intNumero1 = Integer.parseInt(numero1.getText().toString());
-        int intNumero2 = Integer.parseInt(numero2.getText().toString());
+        int intNumero1 = Integer.parseInt(numero1.getText();
+        int intNumero2 = Integer.parseInt(numero2.getText();
         int subtracao = intNumero1 - intNumero2;
         String resultadoSubtracao = String.valueOf(subtracao);
         resultado.setText(resultadoSubtracao);
     }
     public void btnDivisao(View v){
         int intNumero1 = Integer.parseInt(numero1.getText().toString());
-        int intNumero2 = Integer.parseInt(numero2.getText().toString());
+        intNumero2 = Integer.parseInt(numero2.getText().toString());
         int divisao = intNumero1 / intNumero2;
         String resultadoDivisao = String.valueOf(divisao);
         resultado.setText(resultadoDivisao);
     }
-    public void btnMultiplicacao(View v){
+    public void btnMultiplicacao{
         int intNumero1 = Integer.parseInt(numero1.getText().toString());
         int intNumero2 = Integer.parseInt(numero2.getText().toString());
         int multiplicacao = intNumero1 * intNumero2;
